87e431f9f349629d0df1e73a2106ad7e9adab7ae
Rian Oliveira <rian@technosoftware.com.br>
2019-09-24 17:10:50 -0300

    Adding CORS

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 const routers = require('./routers');
 
 const PORT = process.env.PORT || 3000;
 
 const server = express();
-// server.use(cors);
+
+server.use(cors());
 server.use(express.json());
 server.use(routers);
 
